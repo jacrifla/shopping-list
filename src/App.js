72975da2f28fcfb87a0bfeaf5089ddc886abc9fe
jacrifla <jaquelini_cris@hotmail.com>
2024-12-28 14:19:11 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Login from './pages/Login';
 import Home from './pages/Home';
+import Signup from './pages/Signup';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route 
+          path="/home" 
+          element={<Home />}
+        />
+      </Routes>
     </Router>
   );
 }
