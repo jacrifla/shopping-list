aa14bf9f7092f4282f62f2f5deae3ae47479a32e
jacrifla <jaquelini_cris@hotmail.com>
2024-12-28 21:01:04 -0300

    add Home

---
+++ src/pages/Home.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import Sidebar from '../components/Sidebar';
+import MainContent from '../components/MainContent';
+import Header from '../components/Header';
+
+const Home = () => {
+  const [lists, setLists] = useState([
+    { id: 1, name: 'Lista de Compras 1', completed: false },
+    { id: 2, name: 'Lista de Compras 2', completed: true },
+  ]);
+
+  const handleAddList = () => {
+    const newList = { id: lists.length + 1, name: `Nova Lista ${lists.length + 1}`, completed: false };
+    setLists([...lists, newList]);
+  };
+
+
+  const handleEdit = (id) => {
+    console.log(`Editar lista com id: ${id}`);
+  };
+
+  const handleDelete = (id) => {
+    setLists(lists.filter((list) => list.id !== id));
+  };
+
+  const handleToggleComplete = (id) => {
+    setLists(
+      lists.map((list) =>
+        list.id === id ? { ...list, completed: !list.completed } : list
+      )
+    );
+  };
+
+  const handleShare = (id) => {
+    console.log(`Compartilhar lista com id: ${id}`);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="container-fluid p-0">
+        <div className="row min-vh-100 p-3">
+
+          <div className="col-3 ">
+            <Sidebar
+              lists={lists}
+              onToggleComplete={handleToggleComplete}
+              onEdit={handleEdit}
+              onDelete={handleDelete}
+              onShare={handleShare}
+              onAddList={handleAddList}
+            />
+          </div>
+
+          <div className="col-9 ">
+            <MainContent />
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Home;
