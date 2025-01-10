b3aaab5857baddc956e55c0316bf89afe396b6ff
jacrifla <jaquelini_cris@hotmail.com>
2025-01-10 19:59:38 -0300

    style

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,61 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ShoppingList from './ShoppingList';
-import ShareModal from './ShareModal';
-import EditModal from './EditModal';
 
-const Sidebar = ({ lists, onToggleComplete, onEdit, onDelete, onShare, onAddList }) => {
-  const [showModal, setShowModal] = useState(false);
-  const [shareToken, setShareToken] = useState('');
-  const [listName, setListName] = useState('');
-  const [editingList, setEditingList] = useState(null);
-
-  const handleShareClick = (listId, name) => {
-    const token = `LIST-TOKEN-${listId}-${Date.now()}`;
-    setShareToken(token);
-    setListName(name);
-    setShowModal(true);
-    onShare(listId);
-  };
-
-  const handleEditClick = (list) => {
-    setEditingList(list);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
+const Sidebar = ({ lists, onToggleComplete, onEdit, onDelete, onShare, onAddList, onSelectList }) => {
   return (
-    <div className="card h-100 shadow-sm border-0">
+    <div className="card shadow-sm border-0 h-100 rounded">
       <div className="card-header border-bottom-0 pb-3 bg-white">
         <h3 className="mb-0 d-flex align-items-center fs-6">
           <i className="bi bi-list-task me-2 fs-5"></i>
           Minhas Listas
         </h3>
       </div>
-      <div className="card-body bg-white">
+      <div className="card-body bg-white rounded">
         <ShoppingList
           lists={lists}
           onToggleComplete={onToggleComplete}
-          onEdit={handleEditClick}
+          onEdit={onEdit}
           onDelete={onDelete}
-          onShare={handleShareClick}
+          onShare={onShare}
           onAddList={onAddList}
+          onSelectList={onSelectList}
         />
       </div>
-      
-      {/* Modals */}
-      <ShareModal showModal={showModal} onClose={handleCloseModal} token={shareToken} listName={listName} />
-      {editingList && (
-        <EditModal
-          list={editingList}
-          onClose={() => setEditingList(null)}
-          onSave={(listId, newName) => {
-            onEdit(listId, newName);
-            setEditingList(null);
-          }}
-        />
-      )}
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,105 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../components/Sidebar';
-import MainContent from '../components/MainContent';
 import Header from '../components/Header';
+import { formatTitleCase } from '../utils/functions';
+import ToastNotification from '../components/ToastNotification';
+import { createList, deleteList, getListsByUserId, markListAsCompleted, updateList } from '../services/shoppingListService';
+import { generateToken } from '../services/shareListAndToken';
+import ShareModal from '../components/ShareModal';
+import EditModal from '../components/EditModal';
 
 const Home = () => {
-  const [lists, setLists] = useState([
-    { id: 1, name: 'Lista de Compras 1', completed: false },
-    { id: 2, name: 'Lista de Compras 2', completed: true },
-  ]);
+  const [lists, setLists] = useState([]);
+  const [toast, setToast] = useState({ show: false, message: '', type: '' });
+  const [userId, setUserId] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [shareToken, setShareToken] = useState('');
+  const [listName, setListName] = useState('');
+  const [editingList, setEditingList] = useState(null);
 
-  const handleAddList = () => {
-    const newList = { id: lists.length + 1, name: `Nova Lista ${lists.length + 1}`, completed: false };
-    setLists([...lists, newList]);
+  useEffect(() => {
+    const userIdFromStorage = localStorage.getItem('userId');
+    if (userIdFromStorage) {
+      setUserId(userIdFromStorage);
+      fetchUserLists(userIdFromStorage);
+    }
+  }, [])
+
+  const fetchUserLists = async (userId) => {
+    try {
+      const response = await getListsByUserId(userId);
+      console.log(userId);
+      
+      setLists(response || []);
+    } catch (error) {
+      console.error('Erro ao buscar listas:', error.message);
+    }
   };
 
-
-  const handleEdit = (id) => {
-    console.log(`Editar lista com id: ${id}`);
+  const handleCreateList = async () => {
+    try {
+      const response = await createList(userId, 'Nova Lista');
+      setLists([...lists, response]);
+    } catch (error) {
+      alert(error.message);
+    }
   };
 
-  const handleDelete = (id) => {
-    setLists(lists.filter((list) => list.id !== id));
+  const handleMarkAsCompleted = async (listId) => {
+    try {
+      await markListAsCompleted(listId);
+
+      setLists((prevLists) =>
+        prevLists.map((list) =>
+          list.list_id === listId ? { ...list, completed_at: true } : list
+        )
+      );
+
+      setToast({ show: true, message: 'Marcada com sucesso!', type: 'success' });
+    } catch (error) {
+      setToast({ show: true, message: 'Erro ao marcar lista.', type: 'danger' });
+    }
   };
 
-  const handleToggleComplete = (id) => {
-    setLists(
-      lists.map((list) =>
-        list.id === id ? { ...list, completed: !list.completed } : list
-      )
-    );
+  const handleDeleteList = async (listId) => {
+    try {
+      await deleteList(listId);
+      setLists((prevLists) => prevLists.filter((list) => list.list_id !== listId));
+      setToast({ show: true, message: 'Lista excluída com sucesso', type: 'success' });
+    } catch (error) {
+      setToast({ show: true, message: 'Erro ao excluir a lista.', type: 'danger' });
+    }
   };
 
-  const handleShare = (id) => {
-    console.log(`Compartilhar lista com id: ${id}`);
+  const handleUpdateList = async (listId, newName) => {
+    try {
+      const formattedName = formatTitleCase(newName);
+      await updateList(listId, newName);
+      setLists(lists.map((list) =>
+        list.list_id === listId ? { ...list, list_name: formattedName } : list
+      ));
+      setToast({ show: true, message: 'Nome da lista atualizado com sucesso!', type: 'success' });
+    } catch (error) {
+      setToast({ show: true, message: 'Erro ao atualizar o nome da lista.', type: 'danger' });
+    }
+  };
+
+  const handleShareClick = async (listId, name) => {
+    try {
+      const tokenData = await generateToken(listId, userId);
+      const token = tokenData.data.token;
+      console.log(token);
+
+      setShareToken(token);
+      setListName(name);
+      setShowModal(true);
+    } catch (error) {
+      setToast({ show: true, message: 'Erro ao gerar o token.', type: 'danger' });
+    }
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
   };
 
   return (
@@ -40,23 +107,39 @@ const Home = () => {
       <Header />
       <div className="container-fluid p-0">
         <div className="row min-vh-100 p-3">
-
           <div className="col-3 ">
             <Sidebar
               lists={lists}
-              onToggleComplete={handleToggleComplete}
-              onEdit={handleEdit}
-              onDelete={handleDelete}
-              onShare={handleShare}
-              onAddList={handleAddList}
+              onToggleComplete={handleMarkAsCompleted}
+              onEdit={(list) => setEditingList(list)}
+              onDelete={handleDeleteList}
+              onShare={handleShareClick}
+              onAddList={handleCreateList}
             />
-          </div>
 
-          <div className="col-9 ">
-            <MainContent />
+            <ShareModal showModal={showModal} onClose={handleCloseModal} token={shareToken} listName={listName} />
+            {editingList && (
+              <EditModal
+                list={editingList}
+                onClose={() => setEditingList(null)}
+                onSave={(listId, newName) => {
+                  handleUpdateList(listId, newName);
+                  setEditingList(null);
+                }}
+              />
+            )}
+          </div>
+          <div className="col-9">
+            
           </div>
         </div>
       </div>
+      <ToastNotification
+        show={toast.show}
+        message={toast.message}
+        type={toast.type}
+        onClose={() => setToast({ ...toast, show: false })}
+      />
 
     </div>
   );
