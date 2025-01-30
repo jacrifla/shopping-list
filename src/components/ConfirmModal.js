7c544b4530633d4d2fe8fdbb8dc5b7764704d9b4
jacrifla <jaquelini_cris@hotmail.com>
2025-01-30 17:17:47 -0300

    alter

--- src/components/ConfirmModal.js
+++ src/components/ConfirmModal.js
@@ -2,39 +2,36 @@ import React from 'react';
 import Button from './Button';
 import Subtitle from './Subtitle';
 
-const ConfirmModal = ({ show, onClose, onConfirm, message }) => {
+const ConfirmModal = ({ show, onClose, onConfirm, message, buttons }) => {
   if (!show) return null;
 
   return (
     <div className="modal d-block" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
       <div className="modal-dialog modal-dialog-centered">
         <div className="modal-content p-3 border-0">
-        <Subtitle
-        className='text-center'
-          text={'Confirmação'}
-        />
+          <Subtitle className="text-center" text={'Confirmação'} />
           <p className="text-center">{message}</p>
           <div className="d-flex justify-content-between mt-4 gap-3">
-            <Button
-              className="btn btn-secondary w-100"
-              onClick={onClose}
-              text="Cancelar"
-              showText={true}
-            />
-            <Button
-              className="btn btn-danger w-100"
-              onClick={() => {
-                onConfirm();
-                onClose();
-              }}
-              text="Confirmar"
-              showText={true}
-            />
+            {buttons.map((button, index) => (
+              <Button
+                key={index}
+                className={`btn ${button.className} w-100`}
+                onClick={() => {
+                  if (button.action) {
+                    onConfirm(button.action);
+                  }
+                  onClose();
+                }}
+                text={button.text}
+                showText={true}
+              />
+            ))}
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 
 export default ConfirmModal;
