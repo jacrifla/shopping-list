95b9ed45876025e85a4fb184ab9a7324d16b44ea
jacrifla <jaquelini_cris@hotmail.com>
2025-01-31 09:09:23 -0300

    fix

--- src/utils/base.js
+++ src/utils/base.js
@@ -1,2 +1,2 @@
 // export const URL_BASE = 'http://localhost:5000';
-export const URL_BASE = 'https://server-list-shop-mom.railway.app';
+export const URL_BASE = 'https://server-listshopmom.up.railway.app';
