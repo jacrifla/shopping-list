6ec8deed0eef9f0230c16e184fa4df87386b9851
jacrifla <jaquelini_cris@hotmail.com>
2025-02-18 17:31:16 -0300

    fix

--- src/components/ItemSuggestionsList.js
+++ src/components/ItemSuggestionsList.js
@@ -1,53 +1,43 @@
-
 import React from "react";
 
 const ItemSuggestionsList = ({ filteredItems, setItemInputs, setIsItemSelected, itemInputs }) => {
     const handleItemSelect = (item) => {
         console.log("Selecionando item:", item);
 
-        // Atualiza os inputs apenas se o item não for o mesmo
         if (item.itemId !== itemInputs.itemId) {
             setItemInputs({
                 ...itemInputs,
                 name: item.itemName,
                 itemId: item.itemId,
                 itemType: "common",
+                brand: item.brandName || "Desconhecida",
+                barcode: item.barcode || "N/A",
             });
             setIsItemSelected(true);
         }
     };
 
-    // Condicionalmente renderizar a lista apenas se houver itens filtrados
     if (filteredItems.length === 0) return null;
 
     return (
         <div className="dropdown">
-            <ul
-                className="list-group mt-2"
-                style={{
-                    position: "absolute",
-                    top: "100%",
-                    left: "0",
-                    maxHeight: "200px",
-                    overflowY: "auto",
-                    zIndex: 1000,
-                    width: "100%",
-                    backgroundColor: "white",
-                    border: "1px solid #ccc",
-                    borderRadius: "4px",
-                }}
-            >
+            <ul className="list-group mt-2 item-suggestions-list">
                 {filteredItems.map((item) => (
                     <li
                         key={item.itemId}
-                        className="list-group-item list-group-item-action"
+                        className="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
                         onClick={() => {
                             handleItemSelect(item);
-                            // Aqui você pode ocultar a lista assim que o item for selecionado
                             setIsItemSelected(true);
                         }}
                     >
-                        {item.itemName}
+                        <div className="item-info">
+                            <strong className="item-name">{item.itemName}</strong>
+                            <small className="text-muted brand">({item.brandName || "Sem marca"})</small>
+                        </div>
+                        <div className="item-info">
+                            <small className="text-muted barcode">{item.barcode || "Sem código"}</small>
+                        </div>
                     </li>
                 ))}
             </ul>
